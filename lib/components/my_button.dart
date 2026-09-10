c98621806d8e7a3795cf21e78be186e9f46ad3da
FelixHeinze <fheinze1993@icloud.com>
2026-09-10 08:18:39 +0200

    erweiterung des buttons um farbe

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -3,13 +3,17 @@ import 'package:flutter/material.dart';
 class MyButton extends StatelessWidget {
   final String text;
   final void Function()? onTap;
+  final Color color;
 
-  const MyButton({super.key, required this.text, required this.onTap});
+  const MyButton({super.key, required this.text, required this.onTap, required this.color});
 
   @override
   Widget build(BuildContext context) { // vordefiniertes Widget für einen Button, der Text und onTap Funktionalität hat
     return ElevatedButton(
       onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+      ),
       child: Container(
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         child: Text(text),
--- lib/main.dart
+++ lib/main.dart
@@ -68,39 +68,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
             const Text('Counter:'),
@@ -121,6 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 _counter = incrementCounterBy(_counter,1);
               });
             },
+            color: Colors.green,
           ),
           MyButton(
             text: '+2',
@@ -129,6 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 _counter = incrementCounterBy(_counter,2);
               });
             },
+            color: Colors.blue,
           ),
           MyButton(
             text: '-',
@@ -137,6 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 _counter = decrementCounterBy(_counter,1);
               });
             },
+            color: Colors.red,
           ),
           MyButton(
             text: '-2',
@@ -144,7 +122,8 @@ class _MyHomePageState extends State<MyHomePage> {
               setState(() {
                 _counter = decrementCounterBy(_counter,2);
               });
-            }  ,
+            },
+            color: Colors.orange,
           ),
           MyButton(
             text: '*2',
@@ -153,6 +132,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 _counter = multiplyCounterBy(_counter, 2);
               });
             },
+            color: Colors.purple,
           ),
           MyButton(
             text: '/2',
@@ -161,6 +141,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 _counter = halfCounter(_counter);
               });
             },
+            color: Colors.teal,
           ),
           
         ],
